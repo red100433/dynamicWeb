1befe1d1a665db79344b1d374d6b1c98ac4e88b8
Daeyun Jang <red100411@naver.com>
2016-04-12 11:31:36 +0900

    Create project for dynamic web

---
+++ helloMVC/src/controller/DoLogin.java
@@ -0,0 +1,66 @@
+package controller;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import beans.Customer;
+import util.CustomerMap;
+
+/**
+ * Servlet implementation class DoLogin
+ */
+@WebServlet("/DoLogin")
+public class DoLogin extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public DoLogin() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String customerid= request.getParameter("customerID");
+		
+		// perform business logic. return a bean as a result
+		
+		CustomerMap service = new CustomerMap();
+		Customer customer = service.findCustomer(customerid);
+		String page = null;
+		
+		if(customer == null)
+		{
+			request.setAttribute("badid", customerid);
+			page = "/jsps/error.jsp";
+		}
+		
+		else
+		{
+			request.setAttribute("customer", customer);
+			page = "/jsps/success.jsp";
+		}
+		
+		RequestDispatcher dispatcher = request.getRequestDispatcher(page);
+		dispatcher.forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
